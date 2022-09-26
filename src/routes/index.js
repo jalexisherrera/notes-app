e3069e0ff34d6d097512b80245fa983072abdd48
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2022-09-26 13:42:34 -0500

    views

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.send('Index');
+    res.render('index');
 });
 
 router.get('/about', (req, res) => {
-    res.send('About');
+    res.render('about');
 })
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users/singin', (req, res) => {
-    res.send('Ingresando a la app');
+router.get('/users/signin', (req, res) => {
+    res.render('users/signin');
 });
 
-router.get('/users/singup', (req, res) => {
-    res.send('Formulario de autenticación');
+router.get('/users/signup', (req, res) => {
+    res.render('users/signup');
 });
 
 module.exports = router;
