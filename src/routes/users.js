eaa295aacb57f1a0b8daac57eb2e3384b574d544
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2022-09-26 22:24:13 -0500

    sign in

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,11 +3,17 @@ const User = require('../models/User');
 const router = express.Router();
 
 const user = require('../models/User');
-
+const passport = require('passport');
 
 router.get('/users/signin', (req, res) => {
     res.render('users/signin');
 });
+
+router.post('/users/signin', passport.authenticate('local', {
+    successRedirect: '/notes',
+    failureRedirect: '/users/signin',
+    failureFlash: true
+}));
 
 router.get('/users/signup', (req, res) => {
     res.render('users/signup');
