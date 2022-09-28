124e979dd76cf7e79d6c63886d59592f714d8e3e
Alexis Herrers <43160572+jalexisherrera@users.noreply.github.com>
2022-09-27 22:13:13 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/notes-db-app')
+mongoose.connect('mongodb+srv://notes-db-app:4x3BJYBMrXQ9A7t@clusternotesapp.hnttryp.mongodb.net/?retryWrites=true&w=majority')
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
